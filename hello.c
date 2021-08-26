904b62a29e4d9367caf2611491fcd65e3b143c4e
Sathvik Reddy Thurpu <sthurpu2@huskers.unl.edu>
2021-08-26 16:45:54 +0000

    added my majors to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/** Author: sathvik reddy
+ * Partner: august gower
+ *         sthurpu2@huskers.unl.edu
+ *         agower2@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("SATHVIK REDDY & AUGUST GOWER\n");
+  printf("COMPUTER SCIENCE\n");
+  return 0;
+}
